e52e7b7fb0c86f46857e1a9b76e2364fb9cd8b55
ye.wang <905317742@qq.com>
2023-05-09 11:13:43 +0800

    fix official Madgwick's bug

--- example_code/madgwick_mahony_c_code/MadgwickAHRS.cpp
+++ example_code/madgwick_mahony_c_code/MadgwickAHRS.cpp
@@ -51,7 +51,7 @@ void MadgwickAHRS::update(float gx, float gy, float gz, float ax, float ay, floa
     float recipNorm;
     float s0, s1, s2, s3;
     float qDot1, qDot2, qDot3, qDot4;
-    float hx, hy;
+    float hx, hy, bx, bz;
     float _2q0mx, _2q0my, _2q0mz, _2q1mx, _2bx, _2bz, _4bx, _4bz, _2q0, _2q1, _2q2, _2q3, _2q0q2, _2q2q3, q0q0, q0q1, q0q2, q0q3, q1q1, q1q2, q1q3, q2q2, q2q3, q3q3;
 
     // Use IMU algorithm if magnetometer measurement invalid (avoids NaN in magnetometer normalisation)
@@ -106,8 +106,10 @@ void MadgwickAHRS::update(float gx, float gy, float gz, float ax, float ay, floa
         // Reference direction of Earth's magnetic field
         hx = mx * q0q0 - _2q0my * q3 + _2q0mz * q2 + mx * q1q1 + _2q1 * my * q2 + _2q1 * mz * q3 - mx * q2q2 - mx * q3q3;
         hy = _2q0mx * q3 + my * q0q0 - _2q0mz * q1 + _2q1mx * q2 - my * q1q1 + my * q2q2 + _2q2 * mz * q3 - my * q3q3;
-        _2bx = sqrtf(hx * hx + hy * hy);
-        _2bz = -_2q0mx * q2 + _2q0my * q1 + mz * q0q0 + _2q1mx * q3 - mz * q1q1 + _2q2 * my * q3 - mz * q2q2 + mz * q3q3;
+        bx = sqrtf(hx * hx + hy * hy);
+        bz = -_2q0mx * q2 + _2q0my * q1 + mz * q0q0 + _2q1mx * q3 - mz * q1q1 + _2q2 * my * q3 - mz * q2q2 + mz * q3q3;
+        _2bx = bx;
+        _2bz = bz;
         _4bx = 2.0f * _2bx;
         _4bz = 2.0f * _2bz;
 
